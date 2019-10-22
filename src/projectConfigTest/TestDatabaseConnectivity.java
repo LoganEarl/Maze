e649c28b4e426ebd70965131570ecfdfba798529
LoganEarl <iralogan42@gmail.com>
2019-10-22 10:19:43 -0700

    Added a simple database test

---
+++ src/projectConfigTest/TestDatabaseConnectivity.java
@@ -0,0 +1,47 @@
+package projectConfigTest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class TestDatabaseConnectivity {
+    private static final String DATA_DIRECTORY = System.getProperty("user.dir").replace("\\", "/") + "/data/";
+    private static final String TEST_DB_FILE = DATA_DIRECTORY + "test.db";
+
+    @Test
+    void testDriver() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            fail("Was now able to find JDBC Driver");
+        }
+    }
+
+    @Test
+    void testEstablishConnection(){
+        String url = "jdbc:sqlite:" + TEST_DB_FILE;
+        try {
+            Connection c = DriverManager.getConnection(url);
+            Assertions.assertNotNull(c);
+            c.close();
+        } catch (SQLException e) {
+            fail("could not connect to test database");
+        }
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    @AfterEach
+    void tearDown(){
+        File f = new File(TEST_DB_FILE);
+        try {
+            f.delete();
+        }catch(Exception ignored){}
+    }
+}
