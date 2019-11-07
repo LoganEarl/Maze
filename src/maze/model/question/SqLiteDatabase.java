dc349ae4daec5a6ed3680169bfd342b80fd4adc0
skozubenko1 <55997642+skozubenko1@users.noreply.github.com>
2019-11-07 08:39:50 -0800

    Update SqLiteDatabase.java

--- src/maze/model/question/SqLiteDatabase.java
+++ src/maze/model/question/SqLiteDatabase.java
@@ -1,5 +1,6 @@
 package maze.model.question;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.sql.*;
 import java.sql.Connection;
@@ -10,8 +11,23 @@ import java.sql.SQLException;
 
 public class SqLiteDatabase implements MazeDatabase {
 	
+    public static final String QUESTION_TABLE = "CREATE TABLE IF NOT EXISTS \"question\" (\r\n" + 
+    		"	\"id\"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\r\n" + 
+    		"	\"question\"	TEXT NOT NULL,\r\n" + 
+    		"	\"type\"	INTEGER NOT NULL DEFAULT 0,\r\n" + 
+    		"	\"keywords\"	TEXT DEFAULT 0\r\n" + 
+    		");";
+    
+    public static final String ANSWER_TABLE = "CREATE TABLE IF NOT EXISTS \"answer\" (\r\n" + 
+    		"	\"id\"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\r\n" + 
+    		"	\"question_id\"	INTEGER NOT NULL,\r\n" + 
+    		"	\"answer\"	TEXT NOT NULL,\r\n" + 
+    		"	\"correct\"	INTEGER NOT NULL\r\n" + 
+    		");";
+	
 	public String fullFileName;
-	Object lock = new Object();
+	public final Object lock = new Object();
+	
 	
 	public SqLiteDatabase(String fullFileName)
 	{
@@ -26,6 +42,9 @@ public class SqLiteDatabase implements MazeDatabase {
 			try
 			{
 				conn = this.openConnection();
+		    	conn.createStatement().execute(QUESTION_TABLE);            
+		    	conn.createStatement().execute(ANSWER_TABLE);            
+
 			}
 			catch(Exception e)
 			{
@@ -52,7 +71,7 @@ public class SqLiteDatabase implements MazeDatabase {
         return rs.next();
 	}
 	
-	public MazeQuestion add(MazeQuestion q)
+	public boolean insert(Question q)
 	{
 		synchronized(lock)
 		{
@@ -62,45 +81,45 @@ public class SqLiteDatabase implements MazeDatabase {
 			{
 				conn = this.openConnection();
 				
-				if(questionExists(conn, q.question))
-					return null;
+				if(questionExists(conn, q.getQuestion()))
+					return false;
 					
 				String keywords = "";
 				
-				if(q.keywords.size() > 0)
-					keywords = String.join(",", q.keywords);
+				if(q.getKeywords().size() > 0)
+					keywords = String.join(",", q.getKeywords());
 				
 			    String sql = "INSERT INTO question(question, type, keywords) VALUES(?, ?, ?)";
 			        
 			      PreparedStatement pstmt = conn.prepareStatement(sql);
-			      pstmt.setString(1, q.question);
-			      pstmt.setInt(2, q.type.getValue());
+			      pstmt.setString(1, q.getQuestion());
+			      pstmt.setInt(2, q.getType().getValue());
 			      pstmt.setString(3, keywords);
 			      pstmt.executeUpdate();
 			      
 			      ResultSet rs = pstmt.getGeneratedKeys();
 			      if(rs.next())
 			      {
-		             q.id = (int) rs.getLong(1);
+		             int id = (int) rs.getLong(1);
 		             
-		             for(MazeAnswer a: q.answers) {
+		             for(MazeAnswer a: q.getAnswers()) {
 		 			    sql = "INSERT INTO answer (question_id, answer, correct) VALUES(?, ?, ?)";
 		 			    
 		 			   pstmt = conn.prepareStatement(sql);
-					      pstmt.setInt(1, q.id);
-					      pstmt.setString(2, a.answer);
-					      pstmt.setInt(3, a.correct ? 1 : 0);
-					      pstmt.executeUpdate();
-
-					      rs = pstmt.getGeneratedKeys();
-					      
-					      if(rs.next()) {
-					    	  a.id = (int) rs.getLong(1);
-					      }
+						  pstmt.setInt(1, q.getId());
+						  pstmt.setString(2, a.answer);
+						  pstmt.setInt(3, a.correct ? 1 : 0);
+						  pstmt.executeUpdate();
+						
+						  rs = pstmt.getGeneratedKeys();
+						  
+						  if(rs.next()) {
+							  a.id = (int) rs.getLong(1);
+						  }
 		             }
 			      }
 			      
-			      return q;
+			      return true;
 			}
 			catch(Exception e)
 			{
@@ -113,7 +132,7 @@ public class SqLiteDatabase implements MazeDatabase {
 			
 		}        
         
-		return null;
+		return false;
 	}
 	
 	public void exceptionHandling(Exception e)
@@ -121,7 +140,7 @@ public class SqLiteDatabase implements MazeDatabase {
 		System.out.println(e.getMessage());
 	}
 	
-	public MazeQuestion remove(int questionId)
+	public boolean delete(int questionId)
 	{		
 		synchronized(lock)
 		{
@@ -140,12 +159,12 @@ public class SqLiteDatabase implements MazeDatabase {
 				closeConnection(conn);
 			}
 			
-			return null;
+			return false;
 		}	
 		
 	}
 	
-	public boolean update(MazeQuestion q)
+	public boolean update(Question q)
 	{
 		synchronized(lock)
 		{
@@ -169,22 +188,110 @@ public class SqLiteDatabase implements MazeDatabase {
 		return false;
 	}
 	
-	
+    public static void createNewTable(Connection conn, String sql) throws SQLException 
+    {        
+    	conn.createStatement().execute(sql);            
+    }
+    
 	@Override
 	public Question getNextQuestion(QuestionType questionType) {
 		return null;
 	}
-
+	
+	public QuestionType toQuestionType(int i) throws Exception
+	{
+		switch(i)
+		{
+			case 0:
+				return QuestionType.MULTIPLE;
+			case 1:
+				return QuestionType.TRUE_FALSE;
+			case 2:
+				return QuestionType.SHORT;
+		}
+		
+		throw new Exception("Not suppoerted Question Type: " + i);
+	}
+	
 	@Override
-	public List<Question> readAllRecords() {
-		return null;
+	public List<Question> readAllRecords() 
+	{
+		List<Question> list = new ArrayList<Question>();
+		
+		synchronized(lock)
+		{
+			Connection conn = null;
+
+			try
+			{
+				conn = this.openConnection();
+				String sql = "SELECT t1.id, t1.question, t1.type, t1.keywords, t2.id as aid, t2.answer, t2.correct " + 
+				             "FROM \"question\" t1, \"answer\" t2 " + 
+						     "WHERE t1.id = t2.question_id";
+				
+			     Statement stmt  = conn.createStatement();
+	             ResultSet rs    = stmt.executeQuery(sql);
+	             int curId = 0;
+	             MazeQuestion q = null;
+	            
+	            while (rs.next()) {
+	            	
+	            	
+	            	int id = rs.getInt("id");
+	            	String question = rs.getString("question");
+	            	int type = rs.getInt("type");
+	            	String keywords = rs.getString("keywords");
+	            	int answerId = rs.getInt("aid");
+	            	String answer = rs.getString("answer");
+	            	int correct = rs.getInt("correct");
+	            	
+	            	if(curId != id)
+	            	{
+	            		if(q != null)
+	            		{
+	            			list.add(q);
+	            		}
+	            		
+	            		curId = id;
+	            		q = new MazeQuestion();
+	            	}
+
+	            	q.id= id;
+	            	q.question = question;
+	            	q.type = toQuestionType(type);
+	            	if(keywords.length() > 0)
+	            	{
+	            		String[] parts = keywords.split(",");
+	            		
+	            		for(String part: parts)
+	            		{
+	            			q.keywords.add(part);
+	            		}
+	            	}
+	            	q.answers.add(new MazeAnswer(answerId, answer, correct != 0));
+	           
+	            }
+
+			}
+			catch(Exception e)
+			{
+				exceptionHandling(e);
+			}
+			finally
+			{
+				closeConnection(conn);
+			}
+				
+		}
+		return list;
+
 	}
 	
     private static String getURL(String fileName){
         return "jdbc:sqlite:" + fileName;
     }
 	
-    Connection openConnection() {
+    public Connection openConnection() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
         }catch (Exception e){
@@ -200,7 +307,7 @@ public class SqLiteDatabase implements MazeDatabase {
             // create a connection to the database
             conn = DriverManager.getConnection(url);
             
-            System.out.println("Connection to SQLite has been established.");
+            // System.out.println("Connection to SQLite has been established.");
             
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -218,10 +325,7 @@ public class SqLiteDatabase implements MazeDatabase {
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
-    }
-    
-    
-    
-    
-    
+    }    
 }
+
+
